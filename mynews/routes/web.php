27d69ef7e4fd22ec36ae446a4e548efaed9f5039
takahiro0808 <tktk0808@gmail.com>
2020-06-29 11:41:29 +0900

    Authの実装

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -16,6 +16,18 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
